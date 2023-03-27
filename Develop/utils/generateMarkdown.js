1ae0386aee62498279d1c28cc004f44526a4b5ef
Brandon Crites <brandoncrites76@gmail.com>
2023-03-27 16:33:05 -0700

    First

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,47 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Table of contents
+  -[Project Description](#Description)
+  -[Usage](#Usage)
+  -[Contributions](#Contributions)
+  -[Installation](#Installation)
+  -[Questions](#Questions)
+  -[License](#License)
+
+  ## Description
+  ${data.description}
+
+  ## Usage
+  ${data.usage}
+
+  ## Installation
+  ${data.installation}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Questions
+  ${data.email}
+  ${data.github}
+
+  ## License
+  ${data.license}
+
+`;
+}
+
+module.exports = generateMarkdown;
