f9400ca6aafc4f267aeec7ffdad366ab71f4dc29
Brandon Crites <brandoncrites76@gmail.com>
2023-03-28 19:46:49 -0700

    Questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,76 @@
 // TODO: Include packages needed for this application
-const generate = require('generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown')
 const inquirer = require('inquirer')
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs')
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions for your project:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information for your project:',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please provide contribution guidelines for your project:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide test instructions for your project:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license does your project use?',
+            choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'Unlicense'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ])
+    .then((answers) => {
+        const readMeData = generateMarkdown(answers);
+        writeToFile('README.md', readMeData)
+    })
+   
+}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, (err)=>{
+        if (err) {
+            throw err;
+        }
+        console.log("README file saved.")
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = data.license;
+  let licenseChoice = '';
+
+  if( licenseType === 'MIT'){
+    licenseChoice = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+  }else if( licenseType === 'GPLv3'){
+    licenseChoice = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+  }else if(licenseType === 'Apache'){
+    licenseChoice = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+  }else if(licenseType === 'BSD'){
+    licenseChoice = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+  }else{
+    licenseChoice = ''
+  }
+
+  
+
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,19 +27,23 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  
+
   ## Table of contents
-  -[Project Description](#Description)
-  -[Usage](#Usage)
-  -[Contributions](#Contributions)
-  -[Installation](#Installation)
-  -[Questions](#Questions)
-  -[License](#License)
+  -[Project Description](#description)
+  -[Usage](#usage)
+  -[Contributions](#contributions)
+  -[Installation](#installation)
+  -[Questions](#questions)
+  -[License](#license)
 
   ## Description
   ${data.description}
@@ -33,14 +56,12 @@ function generateMarkdown(data) {
 
   ## Contributing
   ${data.contributing}
-
+  
+  
   ## Questions
-  ${data.email}
-  ${data.github}
-
-  ## License
-  ${data.license}
-
+  
+  
+  If you have any questions contact me on [Github] at (https://github.com/${data.github}) or email me at ${data.email}.
 `;
 }
 
